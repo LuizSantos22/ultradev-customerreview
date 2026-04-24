704eb85c8aac90991ba6a97d66464d902905a4ca
LuizSantos22 <83614706+LuizSantos22@users.noreply.github.com>
2026-04-23 23:08:29 -0300

    Create IndexController for customer reviews
    Implement IndexController with index action to load layout and set page metadata.

---
+++ app/code/community/UltraDev/CustomerReview/controllers/IndexController.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * UltraDev_CustomerReview
+ *
+ * @category  UltraDev
+ * @package   UltraDev_CustomerReview
+ * @author    UltraDev
+ * @license   MIT
+ */
+class UltraDev_CustomerReview_IndexController extends Mage_Core_Controller_Front_Action
+{
+    public function indexAction(): void
+    {
+        $this->loadLayout();
+
+        $storeName = Mage::helper('ultradev_customerreview')->getStoreName();
+
+        /** @var Mage_Page_Block_Html_Head $head */
+        $head = $this->getLayout()->getBlock('head');
+        if ($head) {
+            $head->setTitle($storeName . ' é um site confiável?');
+            $head->setDescription(
+                'Veja avaliações reais de clientes da loja ' . $storeName . '. '
+                . 'Opiniões de quem já comprou.'
+            );
+        }
+
+        $this->renderLayout();
+    }
+}
